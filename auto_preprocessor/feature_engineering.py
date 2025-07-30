524ac5b1a6c84b322c5c3e90cb445304015cdfba
Vishesh Srivastava <srivastava.vishesh9@gmail.com>
2025-07-30 18:01:53 +0200

    Improve inline comments

--- auto_preprocessor/feature_engineering.py
+++ auto_preprocessor/feature_engineering.py
@@ -1,6 +1,10 @@
 from sklearn.decomposition import PCA
 
+
 def apply_pca(df, n_components):
+    """Apply PCA and return the principal components."""
+    # initialize PCA transformer with the requested number of components
     pca = PCA(n_components=n_components)
+    # compute the component representation
     components = pca.fit_transform(df)
     return components
